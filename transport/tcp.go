bb42b183315272348bd2e0ddb1394b0f2dbb05f0
Qi Gao <qi.gao2012@gmail.com>
2026-08-20 14:16:02 +0800

    feat: add production TCP socket configuration

---
+++ transport/tcp.go
@@ -0,0 +1,89 @@
+package transport
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"strconv"
+
+	"github.com/qigao/ogrenet"
+)
+
+func (e *Engine) dialTCP(ctx context.Context, endpoint ogrenet.Endpoint) (*net.TCPConn, error) {
+	dialer := net.Dialer{}
+	if e.cfg.tcp.KeepAlive {
+		dialer.KeepAlive = e.cfg.tcp.KeepAlivePeriod
+	} else {
+		dialer.KeepAlive = -1
+	}
+	raw, err := dialer.DialContext(ctx, "tcp", endpoint.Address())
+	if err != nil {
+		return nil, err
+	}
+	tcp, ok := raw.(*net.TCPConn)
+	if !ok {
+		_ = raw.Close()
+		return nil, fmt.Errorf("transport: tcp dial returned %T", raw)
+	}
+	if err := e.configureTCP(tcp); err != nil {
+		_ = tcp.Close()
+		return nil, err
+	}
+	return tcp, nil
+}
+
+func (e *Engine) listenTCP(ctx context.Context, endpoint ogrenet.Endpoint) (*net.TCPListener, error) {
+	lc := net.ListenConfig{}
+	raw, err := lc.Listen(ctx, "tcp", endpoint.Address())
+	if err != nil {
+		return nil, err
+	}
+	ln, ok := raw.(*net.TCPListener)
+	if !ok {
+		_ = raw.Close()
+		return nil, fmt.Errorf("transport: tcp listen returned %T", raw)
+	}
+	return ln, nil
+}
+
+func (e *Engine) configureTCP(conn *net.TCPConn) error {
+	if err := conn.SetNoDelay(e.cfg.tcp.NoDelay); err != nil {
+		return fmt.Errorf("transport: set TCP_NODELAY: %w", err)
+	}
+	if err := conn.SetKeepAlive(e.cfg.tcp.KeepAlive); err != nil {
+		return fmt.Errorf("transport: set TCP keepalive: %w", err)
+	}
+	if e.cfg.tcp.KeepAlive {
+		if err := conn.SetKeepAlivePeriod(e.cfg.tcp.KeepAlivePeriod); err != nil {
+			return fmt.Errorf("transport: set TCP keepalive period: %w", err)
+		}
+	}
+	if e.cfg.tcp.ReadBufferBytes > 0 {
+		if err := conn.SetReadBuffer(e.cfg.tcp.ReadBufferBytes); err != nil {
+			return fmt.Errorf("transport: set TCP read buffer: %w", err)
+		}
+	}
+	if e.cfg.tcp.WriteBufferBytes > 0 {
+		if err := conn.SetWriteBuffer(e.cfg.tcp.WriteBufferBytes); err != nil {
+			return fmt.Errorf("transport: set TCP write buffer: %w", err)
+		}
+	}
+	return nil
+}
+
+func boundEndpoint(endpoint ogrenet.Endpoint, addr net.Addr) ogrenet.Endpoint {
+	host, portText, err := net.SplitHostPort(addr.String())
+	if err != nil {
+		return endpoint
+	}
+	port, err := strconv.ParseUint(portText, 10, 16)
+	if err != nil {
+		return endpoint
+	}
+	endpoint.Port = uint16(port)
+	if endpoint.Host != "" && endpoint.Host != "0.0.0.0" && endpoint.Host != "::" {
+		return endpoint
+	}
+	endpoint.Host = host
+	return endpoint
+}
