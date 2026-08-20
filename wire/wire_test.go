8dd6c7d4b69d5e16c980f6a6d180bf37ffdc91f4
Qi Gao <qi.gao2012@gmail.com>
2026-08-20 14:02:28 +0800

    test: reject invalid headers before payload buffering

--- wire/wire_test.go
+++ wire/wire_test.go
@@ -2,6 +2,7 @@ package wire
 
 import (
 	"bytes"
+	"encoding/binary"
 	"errors"
 	"testing"
 
@@ -109,4 +110,67 @@ func TestUnsupportedFlagsRejected(t *testing.T) {
 	if _, err := codec.Decode(frame); !errors.Is(err, ErrUnsupportedFlags) {
 		t.Fatalf("got %v, want ErrUnsupportedFlags", err)
 	}
+}
+
+func TestInvalidEncryptedHeaderRejectedBeforePayloadArrives(t *testing.T) {
+	header := makeHeader(FlagEncrypted, secure.AlgAESGCM, DefaultMaxPayload)
+	if _, _, err := New(nil).DecodeOne(header); !errors.Is(err, ErrMissingCipher) {
+		t.Fatalf("missing cipher header = %v, want ErrMissingCipher", err)
+	}
+
+	cipher, err := secure.NewAESGCM([]byte("0123456789abcdef0123456789abcdef"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	header = makeHeader(FlagEncrypted, secure.AlgSM4GCM, DefaultMaxPayload)
+	if _, _, err := New(cipher).DecodeOne(header); !errors.Is(err, ErrAlgorithmMismatch) {
+		t.Fatalf("mismatched cipher header = %v, want ErrAlgorithmMismatch", err)
+	}
+}
+
+func TestUnencryptedFrameCannotAdvertiseCipherAlgorithm(t *testing.T) {
+	header := makeHeader(0, secure.AlgAESGCM, 0)
+	if _, _, err := New(nil).DecodeOne(header); !errors.Is(err, ErrAlgorithmMismatch) {
+		t.Fatalf("DecodeOne = %v, want ErrAlgorithmMismatch", err)
+	}
+	if _, err := ParseHeader(header); !errors.Is(err, ErrAlgorithmMismatch) {
+		t.Fatalf("ParseHeader = %v, want ErrAlgorithmMismatch", err)
+	}
+}
+
+func TestMaxPayloadBoundsPlaintextAndDecodedPayload(t *testing.T) {
+	codec := New(nil)
+	codec.MaxPayload = 4
+	if _, err := codec.Encode(ogrenet.Bin([]byte("12345"))); !errors.Is(err, ErrFrameTooLarge) {
+		t.Fatalf("oversized plaintext Encode = %v, want ErrFrameTooLarge", err)
+	}
+
+	expander := expandingCipher{}
+	codec = New(expander)
+	codec.MaxPayload = 4
+	frame := makeHeader(FlagEncrypted, expander.Algorithm(), 1)
+	frame = append(frame, 0x01)
+	if _, err := codec.Decode(frame); !errors.Is(err, ErrFrameTooLarge) {
+		t.Fatalf("expanded plaintext Decode = %v, want ErrFrameTooLarge", err)
+	}
+}
+
+func makeHeader(flags uint8, algorithm secure.Algorithm, length uint32) []byte {
+	header := make([]byte, HeaderSize)
+	binary.BigEndian.PutUint16(header[0:2], Magic)
+	header[2] = Version
+	header[3] = flags
+	binary.BigEndian.PutUint16(header[4:6], uint16(algorithm))
+	binary.BigEndian.PutUint32(header[6:10], length)
+	return header
+}
+
+type expandingCipher struct{}
+
+func (expandingCipher) Algorithm() secure.Algorithm { return secure.AlgLegacyAES128CFB }
+func (expandingCipher) Seal(dst, plaintext []byte) ([]byte, error) {
+	return append(dst, plaintext...), nil
+}
+func (expandingCipher) Open(dst, _ []byte) ([]byte, error) {
+	return append(dst, []byte("12345")...), nil
 }
