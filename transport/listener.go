4cb154e27b51957b16e0c0f590d471fafc01f088
Qi Gao <qi.gao2012@gmail.com>
2026-08-20 13:18:52 +0800

    fix: make listener Done a true shutdown barrier

--- transport/listener.go
+++ transport/listener.go
@@ -16,9 +16,11 @@ type listener struct {
 	handler ogrenet.Handler
 	ctx     context.Context
 	cancel  context.CancelFunc
+	closing chan struct{}
 	done    chan struct{}
 
 	closeOnce sync.Once
+	finalOnce sync.Once
 	errMu     sync.RWMutex
 	err       error
 }
@@ -34,26 +36,25 @@ func (l *listener) Err() error {
 }
 
 func (l *listener) Close() error {
-	return l.closeWithError(nil)
+	return l.initiateClose(nil)
 }
 
 func (l *listener) watchContext() {
 	select {
 	case <-l.ctx.Done():
-		_ = l.closeWithError(l.ctx.Err())
+		_ = l.initiateClose(l.ctx.Err())
 	case <-l.done:
 	}
 }
 
 func (l *listener) acceptLoop() {
+	defer l.finalize()
 	var delay time.Duration
 	for {
 		raw, err := l.ln.Accept()
 		if err != nil {
-			select {
-			case <-l.done:
+			if l.isClosing() {
 				return
-			default:
 			}
 
 			if ne, ok := err.(net.Error); ok && ne.Temporary() {
@@ -69,7 +70,7 @@ func (l *listener) acceptLoop() {
 				select {
 				case <-timer.C:
 					continue
-				case <-l.done:
+				case <-l.closing:
 					if !timer.Stop() {
 						select {
 						case <-timer.C:
@@ -80,7 +81,7 @@ func (l *listener) acceptLoop() {
 				}
 			}
 
-			_ = l.closeWithError(normalizeListenerError(err))
+			_ = l.initiateClose(normalizeListenerError(err))
 			return
 		}
 		delay = 0
@@ -88,33 +89,52 @@ func (l *listener) acceptLoop() {
 		if _, err := l.engine.adopt(raw, l.handler); err != nil {
 			_ = raw.Close()
 			if errors.Is(err, ErrClosed) {
-				_ = l.closeWithError(nil)
+				_ = l.initiateClose(nil)
 			} else {
-				_ = l.closeWithError(err)
+				_ = l.initiateClose(err)
 			}
 			return
 		}
 	}
 }
 
-func (l *listener) closeWithError(cause error) error {
+func (l *listener) initiateClose(cause error) error {
 	var closeErr error
 	l.closeOnce.Do(func() {
+		cause = normalizeListenerError(cause)
+		l.errMu.Lock()
+		l.err = cause
+		l.errMu.Unlock()
+		close(l.closing)
 		l.cancel()
 		closeErr = l.ln.Close()
 		if errors.Is(closeErr, net.ErrClosed) {
 			closeErr = nil
 		}
 		if cause == nil && closeErr != nil {
-			cause = closeErr
+			l.errMu.Lock()
+			l.err = closeErr
+			l.errMu.Unlock()
 		}
-		l.errMu.Lock()
-		l.err = cause
-		l.errMu.Unlock()
+	})
+	return closeErr
+}
+
+func (l *listener) finalize() {
+	l.finalOnce.Do(func() {
+		_ = l.initiateClose(nil)
 		l.engine.removeListener(l)
 		close(l.done)
 	})
-	return closeErr
+}
+
+func (l *listener) isClosing() bool {
+	select {
+	case <-l.closing:
+		return true
+	default:
+		return false
+	}
 }
 
 func normalizeListenerError(err error) error {
