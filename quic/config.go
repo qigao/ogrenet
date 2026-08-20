20d5cc1fb9ef07fffbe7a91f063591aa6e3ec263
Qi Gao <qi.gao2012@gmail.com>
2026-08-20 23:56:14 +0800

    refactor: share QUIC client policy

--- quic/config.go
+++ quic/config.go
@@ -2,27 +2,16 @@ package quic
 
 import (
 	"crypto/tls"
-	"errors"
 	"time"
 
 	quicgo "github.com/quic-go/quic-go"
-)
-
-const (
-	defaultHandshakeTimeout = 5 * time.Second
-	defaultIdleTimeout      = 30 * time.Second
-
-	defaultMaxIncomingStreams             = 32
-	defaultInitialStreamReceiveWindow     = 512 << 10
-	defaultMaxStreamReceiveWindow         = 4 << 20
-	defaultInitialConnectionReceiveWindow = 1 << 20
-	defaultMaxConnectionReceiveWindow     = 8 << 20
+	"github.com/qigao/ogrenet/internal/quicpolicy"
 )
 
 var (
-	ErrALPNRequired   = errors.New("quic: ALPN is required")
-	ErrInvalidTimeout = errors.New("quic: timeout must not be negative")
-	ErrTLSVersion     = errors.New("quic: TLS minimum version must be TLS 1.3 or newer")
+	ErrALPNRequired   = quicpolicy.ErrALPNRequired
+	ErrInvalidTimeout = quicpolicy.ErrInvalidTimeout
+	ErrTLSVersion     = quicpolicy.ErrTLSVersion
 )
 
 // Config contains the intentionally small client-side QUIC policy surface.
@@ -41,47 +30,13 @@ type Config struct {
 }
 
 func (c Config) build() (*tls.Config, *quicgo.Config, error) {
-	if c.ALPN == "" {
-		return nil, nil, ErrALPNRequired
-	}
-	if c.HandshakeTimeout < 0 || c.IdleTimeout < 0 {
-		return nil, nil, ErrInvalidTimeout
-	}
-
-	tlsConfig := &tls.Config{}
-	if c.TLSConfig != nil {
-		tlsConfig = c.TLSConfig.Clone()
-	}
-	if tlsConfig.MinVersion != 0 && tlsConfig.MinVersion < tls.VersionTLS13 {
-		return nil, nil, ErrTLSVersion
-	}
-	if tlsConfig.MaxVersion != 0 && tlsConfig.MaxVersion < tls.VersionTLS13 {
-		return nil, nil, ErrTLSVersion
-	}
-	if tlsConfig.MinVersion == 0 {
-		tlsConfig.MinVersion = tls.VersionTLS13
-	}
-	tlsConfig.NextProtos = []string{c.ALPN}
-
-	handshakeTimeout := c.HandshakeTimeout
-	if handshakeTimeout == 0 {
-		handshakeTimeout = defaultHandshakeTimeout
-	}
-	idleTimeout := c.IdleTimeout
-	if idleTimeout == 0 {
-		idleTimeout = defaultIdleTimeout
-	}
-
-	return tlsConfig, &quicgo.Config{
-		HandshakeIdleTimeout:           handshakeTimeout,
-		MaxIdleTimeout:                 idleTimeout,
-		InitialStreamReceiveWindow:     defaultInitialStreamReceiveWindow,
-		MaxStreamReceiveWindow:         defaultMaxStreamReceiveWindow,
-		InitialConnectionReceiveWindow: defaultInitialConnectionReceiveWindow,
-		MaxConnectionReceiveWindow:     defaultMaxConnectionReceiveWindow,
-		MaxIncomingStreams:             defaultMaxIncomingStreams,
-		MaxIncomingUniStreams:          -1,
-		Allow0RTT:                      false,
-		EnableDatagrams:                c.EnableDatagrams,
-	}, nil
+	return quicpolicy.Build(quicpolicy.Config{
+		TLSConfig:             c.TLSConfig,
+		ALPN:                  c.ALPN,
+		HandshakeTimeout:      c.HandshakeTimeout,
+		IdleTimeout:           c.IdleTimeout,
+		EnableDatagrams:       c.EnableDatagrams,
+		MaxIncomingStreams:    quicpolicy.DefaultMaxIncomingStreams,
+		MaxIncomingUniStreams: -1,
+	})
 }
