9f4303ad8b25efc5c5e3d12587aae31d70f15b0e
Qi Gao <qi.gao2012@gmail.com>
2026-08-21 16:11:37 +0800

    transport: complete Engine lifecycle at drain barrier

--- transport/engine_tracking_remove.go
+++ transport/engine_tracking_remove.go
@@ -43,7 +43,8 @@ func removeTracked[T comparable](e *Engine, m map[T]struct{}, v T) {
 }
 
 func (e *Engine) maybeDoneLocked() {
-	if e.closed && e.activeOps == 0 && len(e.streamListeners) == 0 && len(e.wsListeners) == 0 && len(e.streams) == 0 && len(e.websockets) == 0 && len(e.packets) == 0 && e.admission.idle() {
+	if e.state != engineRunning && e.activeOps == 0 && len(e.streamListeners) == 0 && len(e.wsListeners) == 0 && len(e.streams) == 0 && len(e.websockets) == 0 && len(e.packets) == 0 && e.admission.idle() {
+		e.state = engineDone
 		e.doneOnce.Do(func() { close(e.done) })
 	}
 }
