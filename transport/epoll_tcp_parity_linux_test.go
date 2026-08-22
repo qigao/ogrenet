f51c7845a92b593419d68f2778f01a3e46e6d48a
Qi Gao <qi.gao2012@gmail.com>
2026-08-22 12:23:41 +0800

    test: run limit stats contracts on epoll TCP

--- transport/epoll_tcp_parity_linux_test.go
+++ transport/epoll_tcp_parity_linux_test.go
@@ -7,3 +7,7 @@ import "testing"
 func TestEpollTCPGracefulContracts(t *testing.T) {
 	runTCPGracefulContracts(t, epollFactory(contractProfile{TCP: true}))
 }
+
+func TestEpollTCPLimitStatsContracts(t *testing.T) {
+	runTCPLimitStatsContracts(t, epollFactory(contractProfile{TCP: true}))
+}
